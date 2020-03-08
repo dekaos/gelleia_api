7eab9af6d6abdd5bd2dbfb2aac7d15f42e3570d5
Anderson Stühler <anderson@stuhler.com.br>
2020-03-08 20:17:49 -0300

    Teams > Creates migration

---
+++ priv/repo/migrations/20200308224112_create_teams.exs
@@ -0,0 +1,14 @@
+defmodule GelleiaApi.Repo.Migrations.CreateTeams do
+  use Ecto.Migration
+
+  def change do
+    create table(:teams, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :name, :string
+      add :user_id, :uuid
+
+      timestamps()
+    end
+
+  end
+end
