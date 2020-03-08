f6d8e940d5da327e7da0c8420a389eaa36d4b22c
Anderson Stühler <anderson@stuhler.com.br>
2020-03-08 20:20:41 -0300

    Tasks > Scaffold

---
+++ lib/gelleia_api/tasks/task.ex
@@ -0,0 +1,21 @@
+defmodule GelleiaApi.Tasks.Task do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tasks" do
+    field :finished, :boolean, default: false
+    field :finished_at, :utc_datetime
+    field :task, :string
+    field :team_id, Ecto.UUID
+    field :user_id, Ecto.UUID
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(task, attrs) do
+    task
+    |> cast(attrs, [:task, :user_id, :team_id, :finished_at, :finished])
+    |> validate_required([:task, :user_id, :team_id, :finished_at, :finished])
+  end
+end
