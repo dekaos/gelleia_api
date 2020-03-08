ce465be887a16fe97d8c9e9924579392d705ec54
Anderson Stühler <anderson@stuhler.com.br>
2020-03-08 16:16:18 -0300

    Users > Implements some validations

--- lib/gelleia_api/accounts/user.ex
+++ lib/gelleia_api/accounts/user.ex
@@ -3,12 +3,14 @@ defmodule GelleiaApi.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field :email, :string
+    field :email, :string, unique: true
     field :first_name, :string
     field :last_name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
+    field :password_confirm, :string, virtual: true
     field :picture, :string
-    field :role, :string
+    field :role, :string, default: "user"
     field :status, :string
 
     timestamps()
@@ -17,15 +19,35 @@ defmodule GelleiaApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :picture, :email, :password_hash, :role, :status])
+    |> cast(attrs, [
+        :first_name,
+        :last_name, 
+        :picture,
+        :email, 
+        :password, 
+        :password_confirmation, 
+        :role,
+        :status
+      ])
     |> validate_required([
       :first_name,
       :last_name,
       :picture,
       :email,
-      :password_hash,
+      :password,
+      :password_confirmation,
       :role,
       :status
     ])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase($1))
+    |> validate_lenght(:password, min: 6, max: 32)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
   end
+  
+  defp hash_password(changeset) do 
+    changeset
+  end  
 end
