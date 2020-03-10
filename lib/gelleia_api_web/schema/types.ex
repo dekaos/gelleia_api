66588701d18dfd1bbfa31970436a0508ac075b0d
Anderson Stühler <anderson@stuhler.com.br>
2020-03-10 08:22:03 -0300

    Types > Import absinthe custom types and scalars

--- lib/gelleia_api_web/schema/types.ex
+++ lib/gelleia_api_web/schema/types.ex
@@ -1,6 +1,8 @@
 defmodule GelleiaApiWeb.Schema.Types do
   use Absinthe.Schema.Notation
 
+  import_types(Absinthe.Type.Custom)
+  import_types(GelleiaApiWeb.Schema.Types.Scalars)
   import_types(GelleiaApiWeb.Schema.Types.UserType)
   import_types(GelleiaApiWeb.Schema.Types.TeamType)
   import_types(GelleiaApiWeb.Schema.Types.TaskType)
